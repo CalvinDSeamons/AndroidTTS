ab0814d4c46e3e1f2f9bb29feeee594da0445a11
calvindseamons <calvindseamons@gmail.com>
2018-04-03 13:30:38 -0600

    Here is some testing code for tts and stt for android studio

---
+++ MainTTS.java
@@ -0,0 +1,139 @@
+package com.example.calvinseamons.apptts;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+import android.widget.Toast;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
+public class MainTTS extends AppCompatActivity {
+    Timer t = new Timer();
+
+    private TextToSpeech mTTS;
+    private EditText mEditText;
+    private Button mButtonSpeak;
+    private TextView txvResult;
+    private String[] test = new String[10];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tts);
+        mButtonSpeak = findViewById(R.id.button_speak);
+        txvResult = (TextView) findViewById(R.id.txvResult);
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.UK);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        mButtonSpeak.setEnabled(true);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+
+        mEditText = findViewById(R.id.edit_text);
+
+        mButtonSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+    }
+
+
+
+    private void speak() {
+
+
+        test[0] = "hello";
+        test[1] = "there";
+        test[2] = "How you";
+        test[3] = "doing";
+        for (int i = 0; i < 4; i++) {
+            mTTS.speak(test[i], TextToSpeech.QUEUE_FLUSH, null);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
+    //--------------Speech to text---------------//
+    public void getSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void getTurd(View view) {
+
+        getSpeechInput();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txvResult.setText(result.get(0));
+                    System.out.println(result.get(0));
+
+                    }
+
+
+                    break;
+                }
+        }
+
+    }
+
+
+
+
